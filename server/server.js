dc444e243d8ed8f3ca00e58a11fc461e46fb7593
ManojGowda2006 <manojgowdan807@gmail.com>
2025-08-13 09:35:20 +0530

    google-login

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const router = require('./routes/route');
+
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true, 
+}));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.use('/api', router);
+// Middleware to parse cookies
+app.use(cookieParser());
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
