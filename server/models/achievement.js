56483f044ab42d9b9b1ec87966c892b361154619
ManojGowda2006 <manojgowdan807@gmail.com>
2025-08-16 14:14:38 +0530

    changed-achievement-schema

--- server/models/achievement.js
+++ server/models/achievement.js
@@ -4,7 +4,7 @@ const achievementSchema = new mongoose.Schema({
   title: { type: String, required: true }, // Achievement name
   description: { type: String, required: true }, // Short details
   category: { type: String, enum: ['Academic', 'Sports', 'Dojo', 'Hackathon', 'Community', 'Other'], required: true },
-  achievedBy: { type: String, required: true }, // Who achieved it
+  achievedBy: { type: mongoose.Schema.Types.ObjectId, required: true }, // User ID who achieved it
   date: { type: Date, required: true }, // Date of achievement
   images: [{ type: String }], // Proofs or photos
   likes: { type: Number, default: 0 }
