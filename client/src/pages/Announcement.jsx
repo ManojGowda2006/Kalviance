c1246b8c560ab39e9b0bfdf61c0b622971f17984
Devansh singh <devanshsingh1974@gmail.com>
2025-09-04 23:02:47 +0530

    updated

--- client/src/pages/Announcement.jsx
+++ client/src/pages/Announcement.jsx
@@ -143,7 +143,7 @@ const Announcement = () => {
                 )}
 
 
-                {/* Recent Announcements */}
+                {/* Recent Announcement */}
                 <div>
                      <h2 className="text-2xl font-bold text-gray-800 mb-4">Recent Announcements</h2>
                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
