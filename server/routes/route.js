3f6471e5633046c61e562af4d83cb9cd0ed0a7d0
Devansh singh <devanshsingh1974@gmail.com>
2025-09-04 22:35:31 +0530

    backend

--- server/routes/route.js
+++ server/routes/route.js
@@ -28,4 +28,5 @@ router.delete('/notes/:id', userAuth, deleteNote);
 router.post('/announcements', userAuth, createAnnouncement);
 router.get('/announcements', userAuth, getAnnouncements);
 
+
 module.exports = router;
