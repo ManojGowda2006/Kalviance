fc2867f9b591d6e347c694f55412da5d7a89bb26
Devansh singh <devanshsingh1974@gmail.com>
2025-09-11 10:15:23 +0530

    updated auth.js

--- server/controllers/Authentication.js
+++ server/controllers/Authentication.js
@@ -46,7 +46,7 @@ const login = async (req, res) => {
     res.cookie("token", appToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", 
-      sameSite: "Strict", 
+      sameSite: "None", 
       maxAge: 7 * 24 * 60 * 60 * 1000 
     });
     
